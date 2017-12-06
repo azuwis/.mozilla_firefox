7a16c2e27c8b3be943a095048938e17a079c9dd7
Zhong Jianxin <azuwis@gmail.com>
2017-12-06 15:01:25 +0800

    Clean up extension config in user.js

--- default/user.js
+++ default/user.js
@@ -108,17 +108,3 @@ user_pref("browser.urlbar.oneOffSearches", false);
 // container tabs
 user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.ui.enabled", true);
-
-// Extension //
-
-// vimfx
-user_pref("extensions.VimFx.config_file_directory", "~/.vimfx");
-
-// session manager
-// backup every 4 hours
-user_pref("extensions.{1280606b-2510-4fe0-97ef-9b5a22eafe30}.backup_every", true);
-user_pref("extensions.{1280606b-2510-4fe0-97ef-9b5a22eafe30}.backup_every_time", 240);
-
-// https everywhere
-user_pref("extensions.https_everywhere._observatory.popup_shown", true);
-user_pref("extensions.https_everywhere.toolbar_hint_shown", true);
