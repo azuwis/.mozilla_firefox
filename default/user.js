34613dabe9a7de0cb2c3d82f9a999922064be28e
Zhong Jianxin <azuwis@gmail.com>
2018-12-06 17:55:24 +0800

    Enable webextensions at amo

--- default/user.js
+++ default/user.js
@@ -120,3 +120,6 @@ user_pref("layers.omtp.enabled", true);
 user_pref("browser.autofocus", false);
 // enable hardware compositing
 //user_pref("layers.acceleration.force-enabled", true);
+// enable webextensions at amo
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
