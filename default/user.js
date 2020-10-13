3549fcce742efb41cff3ac13fc06af656e069203
Zhong Jianxin <azuwis@gmail.com>
2020-10-13 20:39:37 +0800

    Enable webrender and vaapi

--- default/user.js
+++ default/user.js
@@ -117,6 +117,10 @@ user_pref("layers.omtp.enabled", true);
 user_pref("browser.autofocus", false);
 // enable hardware compositing
 user_pref("layers.acceleration.force-enabled", true);
+// enable webrender
+user_pref("gfx.webrender.all", true);
+// enable vaapi
+user_pref("media.ffmpeg.vaapi.enabled", true);
 // enable webextensions at amo
 //user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 //user_pref("extensions.webextensions.restrictedDomains", "");
